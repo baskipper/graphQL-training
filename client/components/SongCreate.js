a85e8f520dacd71a2ad947cd96c62af7d95672b8
bskipper <baskipper67@gmail.com>
2020-08-12 15:33:29 -0500

    Queries are now refetched after creation

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react'
 import {graphql} from "react-apollo";
+import {Link, hashHistory} from "react-router";
 import gql from 'graphql-tag'
+
+import query from '../queries/fetchSongs'
 
 class SongCreate extends Component {
     constructor(props) {
@@ -14,13 +17,15 @@ class SongCreate extends Component {
         this.props.mutate({
             variables: {
                 title: this.state.title
-            }
-        })
+            },
+            refetchQueries: [{query}]
+        }).then(() => hashHistory.push('/'))
     }
 
     render() {
         return (
             <div>
+                <Link to={'/'}>Back</Link>
                 <h3>Create a new song</h3>
                 <form onSubmit={this.onSubmit.bind(this)}>
                     <label>Song Title:</label>
